911f4a18a890367f4fcfb7b6393eb23b7ea29907
Gonçalves, Marcelo <tchelojg@gmail.com>
2021-02-07 19:25:46 -0300

    Adding output_data.js file.

---
+++ output_data.js
@@ -0,0 +1,4 @@
+document.getElementById("subtitle").innerHTML = "revisiting the fundamentals";
+document.write('<h3 style="text-align: center;">outputting data on the screen</h3>');
+window.alert("Using a window to output data");
+console.log("Outputting data to the console");
