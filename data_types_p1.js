283da02f6d027669d3164eb542f1491df675629e
Gonçalves, Marcelo <tchelojg@gmail.com>
2021-02-07 20:59:15 -0300

    data types part 1

---
+++ data_types_p1.js
@@ -0,0 +1,9 @@
+var user_name = "marcelo";
+var age = 30;
+var isMale = true;
+var country;
+
+console.log(typeof user_name);
+console.log(typeof age);
+console.log(typeof isMale);
+console.log(typeof country);
