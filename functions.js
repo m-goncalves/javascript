08debcb2379b25241304806d925840fe5d20f0b6
Gonçalves, Marcelo <tchelojg@gmail.com>
2021-02-07 22:08:29 -0300

    functions

--- functions.js
+++ functions.js
@@ -1,14 +1,13 @@
+//changeContent("subtitle", "revisiting the fundamentals");
+
+result = add(5, 5);
+console.log(result);
+
 function changeContent(element, content) {
     document.getElementById(element).innerHTML = content;
 }
-
-changeContent("subtitle", "revisiting the fundamentals");
-changeContent("title", "Javascript Training");
 
 function add(num1, num2){
     let total = num1 + num2;
     return total;
 }
-
-result = add(5, 5);
-console.log(result);
