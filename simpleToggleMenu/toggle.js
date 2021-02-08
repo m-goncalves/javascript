c079f4414595c6bb92ca525145da271a08cd3518
Gonçalves, Marcelo <tchelojg@gmail.com>
2021-02-08 11:59:51 -0300

    simple toggle menu

---
+++ simpleToggleMenu/toggle.js
@@ -0,0 +1,9 @@
+function toggleMenu(){
+
+    let menuArea = document.getElementById("menu-area");
+    if(menuArea.style.width == "200px"){
+        menuArea.style.width = "0px";
+    }else {
+        menuArea.style.width = "200px";
+    }
+};
