c6da70b2faade69c9fea444dfe53c81c03084f0e
Gonçalves, Marcelo <tchelojg@gmail.com>
2021-02-09 17:22:20 -0300

    string manipulation

---
+++ string_manipulation.js
@@ -0,0 +1,12 @@
+let text = document.querySelector("h1").textContent;
+console.log(text);
+console.log(text.length);
+console.log(text.indexOf("Training"));
+console.log(text.substr(9)); 
+
+let txt = document.querySelector("h2").textContent;
+console.log(txt.replace("basics", "fundamentals"));
+console.log(txt.toUpperCase());
+user_name = "    marcelo    ";
+console.log(user_name.trim());
+console.log(text.split(" "));
